c4a7b58d483af40d66aa77cf0cd8bb4b05991eb7
YOUSSEF-YL <youssef.bachar7@gmail.com>
2024-06-07 10:34:43 +0100

    bg

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 const App = () => {
   return (
     <div>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div>
       <Navbar />
     </div>
   );
