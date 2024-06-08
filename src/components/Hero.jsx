cbb016f44f65d0360d8a4aa27901762df6fbc28f
YOUSSEF-YL <youssef.bachar7@gmail.com>
2024-06-08 11:38:10 +0100

    about

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
         </div>
         <div className='w-full lg:w-1/2 lg:p-8'>
           <div className='flex justify-center'>
-            <img src="/src/assets/Profile.png" alt="" />
+            <img src="/src/assets/Profile.png" alt="youssef bachar" />
           </div>
         </div>
       </div>
